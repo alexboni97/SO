ff6a01f651f3fe7675a23fd74116d6dee36b1922
alexboni97 <boni@DESKTOP-A79GDSH>
2024-05-06 02:47:22 +0200

    wrong answer

--- ficheros_p6/cocinero.c
+++ ficheros_p6/cocinero.c
@@ -1,13 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h> /* For mode constants */
+#include <fcntl.h>	  /* For O_* constants */
+#include <semaphore.h>
+#include <signal.h>
 
-#define M 10
+#define M 10 //raciones que puede servir del caldero
+#define MAX_DATOS_MEM_COMPARTIDA 100 //*sizeof(int) //raciones que puede servir del caldero
+
+int *buf;
+sem_t *racionesPreparadas;
+sem_t *calderoVacio;
 
 int finish = 0;
 
 void putServingsInPot(int servings)
 {
+	//FOR SERVINGS SEMPOST RACINESPREPARADAS
 }
 
 void cook(void)
@@ -24,6 +35,30 @@ void handler(int signo)
 
 int main(int argc, char *argv[])
 {
+	int shmC;
+	signal(SIGINT,handler);
+	signal(SIGTERM,handler);
 
+	shmC=shm_open("/BUFFERC",O_CREAT|O_RDWR,S_IRUSR | S_IWUSR);// PREGUNTAR POR ESTOS FLAGS
+	ftruncate(shmC,MAX_DATOS_MEM_COMPARTIDA*sizeof(int));
+
+	buf=(int *)nmap(NULL, MAX_DATOS_MEM_COMPARTIDA*sizeof(int),PROT_WRITE,MAP_SHARED,shdC,0);
+	*buf=0;
+	
+	//abrir semaforos
+	racionesPreparadas=sem_open("/RACIONESP",O_CREAT, 0700,0);
+	racionesPreparadas=sem_open("/VACIOCAL",O_CREAT, 0700,0);
+
+	//cocinar
+	cook();
+
+	//cerrar semaforos y desmapeo
+	munmap(buf,MAX_DATOS_MEM_COMPARTIDA*sizeof(int));
+	close(shmC);
+	unlink("/BUFFERC");
+	sem_close(racionesPreparadas);
+	sem_close(calderoVacio);
+	sem_unlink("/RACIONESP");
+	sem_unlink("VACIOCAL");
 	return 0;
 }
--- ficheros_p6/salvajes.c
+++ ficheros_p6/salvajes.c
@@ -1,12 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h> /* For mode constants */
+#include <fcntl.h>	  /* For O_* constants */
+#include <semaphore.h>
+#include <signal.h>
 
 #define NUMITER 3
 
 int getServingsFromPot(void)
 {
+	//INTENTO SERVIRME 
 
+	//SI ESTA EL CALDERO VACIO NOTIFICO
+
+	//ESPERO QUE ME SIRVAN LA RACION
+	//MENORO UNA RACION AL COCINERO
 }
 
 void eat(void)
@@ -18,11 +28,18 @@ void eat(void)
 
 void savages(void)
 {
-
+	//PIDO COMIDA Y DESPUES COMO
 }
 
 int main(int argc, char *argv[])
 {
+	//ABRO SEMAFORO COCINERO SINO ERROR
+
+	//ABRO SEMAFOROS Y MAPEO A LA MEMORIA COMPARTIDA
+
+	//HAGO QUE COMA EL SALVAJE
+
+	//LIBERO Y CIERRO SEMAFOROS
 
 	return 0;
 }
